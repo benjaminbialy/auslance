74e2df6a8523e8b4d812ffcc518c1b762a2e8814
Benjamin Bialy <benjamincbialy@gmail.com>
2022-08-16 17:21:08 +1000

    started creating logic for filtering component

--- components/Jobs/JobFilters.tsx
+++ components/Jobs/JobFilters.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function JobFilters() {
-  return <div>JobFilters</div>;
+function JobFilters({ savedFilters }) {
+  const [filterID, setFilterID] = useState(-1);
+  return (
+    <div>
+      {savedFilters.map((filter: JobFilterInterface) => (
+        <div
+          className={filterID == filter.id ? "text-green-400" : "text-red-400"}
+          onClick={() => setFilterID(filter.id)}
+        >
+          {filter.name}
+        </div>
+      ))}
+      {filterID > -1 ? (
+        <div>
+          <div>Titles/Roles</div>
+          <div>Location</div>
+          <div>more filters</div>
+        </div>
+      ) : (
+        <div>Create a filter</div>
+      )}
+    </div>
+  );
 }
 
 export default JobFilters;
+
+interface JobFilterInterface {
+  id: number;
+  name: string;
+  location?: string;
+  minimum?: number;
+  maximum?: number;
+  roles?: Array<{ roleName: string; roleID: number }>;
+}
