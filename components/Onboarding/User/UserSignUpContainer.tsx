e613f2e4719467b521e2581474ba5539412b25bb
Benjamin Bialy <benjamincbialy@gmail.com>
2022-08-25 10:10:42 +1000

    created onboarding pages

---
+++ components/Onboarding/User/UserSignUpContainer.tsx
@@ -0,0 +1,7 @@
+import React, { FC, useState } from "react";
+
+export const UserSignUpContainer: FC = () => {
+  const [count, setCount] = useState(0);
+
+  return <div>UserSignUpContainer</div>;
+};
