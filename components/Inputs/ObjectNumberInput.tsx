1c814efdd9b990cf2351d76407f048f4fb82b094
Benjamin Bialy <benjamincbialy@gmail.com>
2022-09-06 15:18:03 +1000

    made object inputs type safe

--- components/Inputs/ObjectNumberInput.tsx
+++ components/Inputs/ObjectNumberInput.tsx
@@ -1,6 +1,20 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, Dispatch, FC, SetStateAction } from "react";
 
-function ObjectNumberInput({
+interface Props {
+  loading: boolean;
+  required: boolean;
+  placeholder: string;
+  field: string;
+  value: Object;
+  setValue: Dispatch<SetStateAction<Object>>;
+  min: number;
+  max?: number;
+  step?: number;
+}
+
+export const ObjectNumberInput: FC<Props> = ({
+  loading,
+  required = false,
   value,
   setValue,
   placeholder,
@@ -8,7 +22,7 @@ function ObjectNumberInput({
   min = 0,
   max = null,
   step = 1,
-}) {
+}) => {
   return (
     <input
       type={"number"}
@@ -16,12 +30,12 @@ function ObjectNumberInput({
       value={value[field]}
       min={min}
       max={max}
-      step={1}
+      step={step}
+      required={required}
+      disabled={loading}
       onChange={(e: ChangeEvent<HTMLInputElement>) =>
         setValue((prev) => ({ ...prev, [field]: e.target.value }))
       }
     />
   );
-}
-
-export default ObjectNumberInput;
+};
--- components/Inputs/ObjectTextInput.tsx
+++ components/Inputs/ObjectTextInput.tsx
@@ -1,15 +1,30 @@
-import React, { ChangeEvent } from "react";
+import React, { Dispatch, FC, SetStateAction, ChangeEvent } from "react";
 
-function ObjectTextInput({ value, setValue, placeholder, field }) {
+interface Props {
+  loading: boolean;
+  required: boolean;
+  placeholder: string;
+  field: string;
+  value: Object;
+  setValue: Dispatch<SetStateAction<Object>>;
+}
+
+export const ObjectTextInput: FC<Props> = ({
+  loading,
+  required = true,
+  placeholder,
+  field,
+  value,
+  setValue,
+}) => {
   return (
     <input
       placeholder={placeholder}
       value={value[field]}
+      required={required}
       onChange={(e: ChangeEvent<HTMLInputElement>) =>
         setValue((prev) => ({ ...prev, [field]: e.target.value }))
       }
     />
   );
-}
-
-export default ObjectTextInput;
+};
