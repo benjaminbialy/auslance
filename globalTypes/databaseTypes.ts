c9912b05cfb717f58c078af5527ae36775473b4b
benjaminbialy <benjamincbialy@gmail.com>
2022-10-02 10:38:08 +1100

    moved types for database fields into global file

---
+++ globalTypes/databaseTypes.ts
@@ -0,0 +1,37 @@
+export enum ExpertiseLevel {
+  beginner = "beginner",
+  intermediate = "intermediate",
+  expert = "expert",
+}
+
+export type Area = "sales" | "engineering" | "marketing" | "design";
+export enum Visibility {
+  private = "private",
+  public = "public",
+}
+
+export enum RemoteLevel {
+  none = "none",
+  hybrid = "hybrid",
+  full = "full",
+}
+
+export enum JobStatus {
+  cancelled = "cancelled",
+  posted = "posted",
+  current = "current",
+  completed = "completed",
+}
+
+export enum JobLength {
+  short = "short",
+  medium = "medium",
+  long = "long",
+}
+
+export enum ProposalStatus {
+  cancelled = "cancelled",
+  rejected = "rejected",
+  sent = "sent",
+  accepted = "accepted",
+}
