43268df0c27c0b153d4f1f6b80f1d94f8def2e2e
benjaminbialy <benjamincbialy@gmail.com>
2022-10-08 18:17:28 +1100

    typed Job

--- components/Jobs/Job.tsx
+++ components/Jobs/Job.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { FC } from "react";
+import JobInterface from "./JobInterface";
 
-function Job({ job }) {
+interface Props {
+  job: JobInterface;
+}
+
+const Job: FC<Props> = ({ job }) => {
   return (
     <div className="border-2 border-gray-300 rounded-2xl">
+      <div className="text-red-500">{job.job_id}</div>
       <div>{job.title}</div>
       <div>{job.location}</div>
-      <div>{job.budget}</div>
-      <div>{job.hoursPerWeek}</div>
-      <div>{job.timePosted}</div>
       <div>{job.description}</div>
+      <div>{job.full_price}</div>
+      <div>{job.remote_level}</div>
       {job.skills.map((skill: string) => (
-        <div key={"skill-" + skill}>{job.skill}</div>
+        <div key={"skill-" + skill}>{skill}</div>
       ))}
-      <div>{job.noProposals}</div>
-      <div>{job.employerReview}</div>
-      <div>{job.employerName}</div>
+      <div>{job.no_proposals}</div>
+      <div>{job.employers.name}</div>
+      <div>{job.employers.location}</div>
     </div>
   );
-}
+};
 
 export default Job;
