9ea1db710ad4e6f9048af321eb34cda585fb655c
benjaminbialy <benjamincbialy@gmail.com>
2022-10-16 09:43:13 +1100

    added padding and margin props

--- components/Wrappers/FlexWrapper.tsx
+++ components/Wrappers/FlexWrapper.tsx
@@ -4,15 +4,25 @@ interface Props {
   children: ReactNode;
   isColumn?: boolean;
   flexProps?: string;
+  colour?: string;
+  margin?: string;
+  padding?: string;
 }
 
 export const FlexWrapper: FC<Props> = ({
   children,
   isColumn = false,
   flexProps = "",
+  colour = "",
+  margin = "",
+  padding = "",
 }) => {
   return (
-    <div className={`flex ${isColumn && "flex-col"} ${flexProps}`}>
+    <div
+      className={`flex ${
+        isColumn && "flex-col"
+      } ${flexProps} ${colour} ${margin} ${padding}`}
+    >
       {children}
     </div>
   );
