1e16d652059cb3546b15b5eb098db01807520eee
benjaminbialy <benjamincbialy@gmail.com>
2022-10-16 11:10:26 +1100

    started creating AngelList inspired JobPreview

--- components/Jobs/JobPreview.tsx
+++ components/Jobs/JobPreview.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 import React, { FC } from "react";
+import { capitaliseFirstLetter } from "../../utils/capitaliseFirstLetter";
+import { getJobLengthMonths } from "../../utils/getJobLengthMonths";
+import { getTimeAgoFromEpoch } from "../../utils/getTimeAgoFromEpoch";
+import { centsToDollars } from "../../utils/moneyConversions";
+import { FlexWrapper } from "../Wrappers/FlexWrapper";
 import JobInterface from "./JobInterface";
 
 interface Props {
@@ -8,12 +13,29 @@ interface Props {
 
 export const JobPreview: FC<Props> = ({ job }) => {
   return (
-    <div className="border-2 border-gray-300 rounded-2xl">
-      <Link href={`jobs/${job.job_id}`}>
+    <div className="border-2 border-gray-300 rounded-md p-6 mb-6">
+      {/* <Link href={`jobs/${job.job_id}`}>
         <a className="text-red-500">{job.job_id}</a>
-      </Link>
-      <div>{job.title}</div>
-      <div>{job.location}</div>
+      </Link> */}
+      <div className="text-lg font-semibold">{job.title}</div>
+      <FlexWrapper>
+        <div>
+          {job.employers.name + ", " + capitaliseFirstLetter(job.location)}
+        </div>
+      </FlexWrapper>
+      <FlexWrapper>
+        <div className="text-gray-600">
+          {job.is_hourly
+            ? `Paying up to $${centsToDollars(job.max_cents_per_hour)} per hour`
+            : `Has a budget of $${centsToDollars(job.max_cents_per_hour)}`}
+        </div>
+        <div className="mx-1 text-gray-600">-</div>
+        <div className="text-gray-600">{getJobLengthMonths(job.length)}</div>
+        <div className="mx-1 text-gray-600">-</div>
+        <div className="text-gray-600">
+          Posted {getTimeAgoFromEpoch(job.time_posted)}
+        </div>
+      </FlexWrapper>
       <div>{job.description}</div>
       <div>{job.full_price}</div>
       <div>{job.remote_level}</div>
@@ -21,8 +43,6 @@ export const JobPreview: FC<Props> = ({ job }) => {
         <div key={"skill-" + skill}>{skill}</div>
       ))}
       <div>{job.no_proposals}</div>
-      <div>{job.employers.name}</div>
-      <div>{job.employers.location}</div>
     </div>
   );
 };
